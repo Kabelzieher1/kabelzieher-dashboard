44265f73a13d1ae4896addaa3fca8a9b65772c0f
Kabelzieher1 <valentin.ritter@kabelziehtrupp.de>
2025-08-14 20:06:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,5 @@ def health():
     return "Kabelzieher Dashboard – läuft ✅"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
